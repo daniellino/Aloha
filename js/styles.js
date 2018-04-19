2bd41ff17d9156d5a0ec3f9abf9fc5e2b620dc62
Daniel Hamidi <hamidiwebdesign@gmail.com>
2018-04-18 19:26:50 -0700

    Updating some class names

--- js/styles.js
+++ js/styles.js
@@ -1,1 +1,5 @@
-console.log("helllllllo");
+$(document).ready(function() {
+    $('.loved-products').css("color", "#e2574c");
+    $('.product-type').css("color", "blue");
+    $('.heavy').css("color", "green");
+});
