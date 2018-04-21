4f930cc2b643a19c579a6c64d00df20d6fd74280
Daniel Hamidi <hamidiwebdesign@gmail.com>
2018-04-20 18:41:53 -0700

    changing some images from html to css as a backgound, creating bottun interactivity, completeing flexbox for product categories,center text, fonts, &

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
 $(document).ready(function() {
+
+    // Flickity
     $('.main-carousel').flickity({
         cellAlign: 'left',
         contain: true,
         wrapAround: true,
         prevNextButtons: false
     });
+
+
+    $('#sign-up-form').on('submit', function(evt) {
+        evt.preventDefault();
+        var userEmail = $('#your-email').val();
+
+        if (userEmail != "") {
+            alert("thank you for sub")
+        } else {
+            alert("wrong!")
+        }
+    });
+
+
 
 });
